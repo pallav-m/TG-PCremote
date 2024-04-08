6eee97eaa14b905e3ad0069d01058c338d5bb6a1
Pallav <pallavmishra314@gmail.com>
2024-04-08 20:40:26 +0530

    directly run command and get output for single line commands

--- bash_func.py
+++ bash_func.py
@@ -1,39 +1,41 @@
 import subprocess
 from decouple import config
 
-base_dir = config('base_dir', default='~/')
+import logging
 
-def is_valid_linux_command(command):
-    try:
-        # Use subprocess to execute the command with the "--help" flag
-        # This will not run the command but just check if it's valid
-        # with open('file.txt', "w") as outfile:
-        #     subprocess.run([*command], stdout=outfile)
-        result = subprocess.run([*command, "--help"], shell=True, check=True, capture_output=True, text=True)
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
 
-        return True
-    except subprocess.CalledProcessError:
-        return False
+
+
+# def is_valid_linux_command(command):
+#     try:
+#         # Use subprocess to execute the command with the "--help" flag
+#         # This will not run the command but just check if it's valid
+#         # with open('file.txt', "w") as outfile:
+#         #     subprocess.run([*command], stdout=outfile)
+#         result = subprocess.run([*command, "--help"], shell=True, check=True, capture_output=True, text=True)
+#
+#         return True
+#     except subprocess.CalledProcessError:
+#         return False
+
 
 def run_command(command):
     input_command = command
 
     try:
-        #subprocess.run(f"cd {base_dir}")
-        result = subprocess.run([*input_command], check=False, capture_output=True, text=True)
-        return result.stdout
+        subprocess.run('cd ~', shell=True)
+        #passing the command as a string, hence shell=True
+        result = subprocess.run(input_command, check=False, capture_output=True, text=True, shell=True)
+        logging.info(result.stdout)
+        logging.info(result.stderr)
+        if(result.stderr == ''):
+            return result.stdout
+        else:
+            return result.stderr
     except:
         return "Unexpected Error"
 
-def main():
-    command = input("Enter a command to check: ").split()
 
-    if is_valid_linux_command(command):
-        print(f"{' '.join(command)} is a valid Linux command.")
-        a = run_command(command)
-        print(a)
-    else:
-        print(f"{command} is not a valid Linux command.")
 
-if __name__ == "__main__":
-    main()
